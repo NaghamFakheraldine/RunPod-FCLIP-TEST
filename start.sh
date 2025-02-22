8142a0883cf4401c5e705fdeafc85dc0895c9010
NaghamFakheraldine <93098110+NaghamFakheraldine@users.noreply.github.com>
2025-02-22 21:31:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,6 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
-# Preload the FashionCLIP model
-echo "Preloading FashionCLIP model"
-python3 -c "from fashion_clip.fashion_clip import FashionCLIP; FashionCLIP('fashion-clip')"
-
 # Start the Runpod worker
 echo "Starting Runpod worker"
-python3 fclip.py
+python3 -c "import runpod; from fclip import handler; runpod.serverless.start({'handler': handler})"
